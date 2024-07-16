7237e658a42936cb53030776bdd297939b664678
Square <aayushk4980@gmail.com>
2024-07-16 11:02:09 +0530

    Displayin cart item in cart when item is added

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import emptyCartIcon from '../images/illustration-empty-cart.svg';
 import { useCartData } from '../context/CartDataContext.jsx';
+import CartItem from './CartItem.jsx';
 
 export default function Cart() {
 
@@ -23,10 +24,16 @@ export default function Cart() {
 
     return (
         <div className="relative flex-grow max-w-96 min-w-80 mr-2">
-            <div className="bg-rose50 sticky top-0 left-0 w-80 min-h-56 max-h-96 m-4 p-4 rounded-lg">
-                <p className="text-redGiven font-bold text-2xl">Your Cart ({totalItemsInCart})</p>
-                <img src={emptyCartIcon} alt="Empty Cart" className="mx-auto mt-6" />
-                <p className="mt-4 text-center text-rose500 font-semibold">Your added items will appear here</p>
+            <div className="bg-rose50 sticky top-0 left-0 w-96 min-h-56 max-h-96 m-4 p-4 rounded-lg">
+                <p className="text-redGiven font-bold text-2xl mb-4">Your Cart ({totalItemsInCart})</p>
+                {
+                    totalItemsInCart <= 0 ? <>
+                        <img src={emptyCartIcon} alt="Empty Cart" className="mx-auto mt-6" />
+                        <p className="mt-4 mb-6 text-center text-rose500 font-semibold">Your added items will appear here</p>
+                    </> : cartItems.map(item => item['count'] > 0 ?
+                        <CartItem key={item['name']} item={item} /> : null
+                    )
+                }
             </div>
         </div>
     );
